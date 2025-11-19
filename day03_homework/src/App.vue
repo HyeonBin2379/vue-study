aa0d17499d68597c59d8fae2ed628e9d4a23693c
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-11-19 19:57:09 +0900

    day03_homework 완료

--- day03_homework/src/App.vue
+++ day03_homework/src/App.vue
@@ -3,6 +3,7 @@ import Practice1 from './components/Practice1.vue';
 import Practice2 from './components/Practice2.vue';
 import Practice3 from './components/Practice3.vue';
 import Practice4 from './components/Practice4.vue';
+import Practice5 from './components/Practice5.vue';
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import Practice4 from './components/Practice4.vue';
   <Practice2 />
   <Practice3 />
   <Practice4 />
+  <Practice5 />
 </template>
 
 <style></style>
---
+++ day03_homework/src/components/Practice5.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const inputValue = ref('');
+const inputRef = ref('');
+
+const clickFocus = computed(() => {
+  inputRef.value.focus();
+  alert(inputValue.value);
+});
+</script>
+
+<template>
+  <div>
+    <h1>Practice5</h1>
+    <input type="text" ref="inputRef" v-model="inputValue" />
+    <button @click="clickFocus">클릭</button>
+  </div>
+</template>
